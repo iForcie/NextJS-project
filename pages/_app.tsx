341706876d63e11dfd958d476a2b450d9ee86947
iForcie <ilyacool3@gmail.com>
2022-06-28 13:24:29 +0300

    Head component and React Fragment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>     
+          <Head>
+            <title>My Top </title>
+            <link key={1} rel="icon" href="/favicon2.ico" />
+          </Head>
+          <Component {...pageProps} />
+        </>;
 }
 
 export default MyApp;
